0ebe481010521587b6f49b4735fdb2a2756c5371
Yuriy Gomenyuk <yourgo@gmail.com>
2023-05-18 20:18:12 +0300

    init

---
+++ app.js
@@ -0,0 +1,54 @@
+require('dotenv').config();
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./utils/swaggerApi.json');
+
+const customErrorMessage = require('./utils/customErrorsMessages');
+
+const { DEV_ENV } = process.env;
+
+const app = express();
+
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get('/api', (req, res) => {
+  res.send(
+    'Hello World! This is the first response from TWEETS backend :-)'
+  );
+});
+
+app.all('*', (req, res) => {
+  res.status(404).json({
+    message: 'route not found',
+  });
+});
+
+/**
+ * * Global error handler (middleware)
+ */
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  const { status } = err; // there we get error status
+
+  if (DEV_ENV === 'development') {
+    const { status: customStatus, message } = customErrorMessage(err.message);
+    res.status(customStatus || 500).json({
+      message,
+      stack: err.stack,
+    });
+  } else {
+    res.status(status || 500).json({
+      message: err.message,
+    });
+  }
+});
+
+module.exports = app;
---
+++ utils/customErrorsMessages.js
@@ -0,0 +1,17 @@
+const customErrorMessage = (message) => {
+  if (!message) {
+    return { status: 500, message: 'internal error' };
+  }
+  if (message.startsWith('E11000') && message.includes('dup key: { name')) {
+    return { status: 409, message: 'name already used' };
+  }
+  if (message.startsWith('E11000') && message.includes('dup key: { email')) {
+    return { status: 409, message: 'email already used' };
+  }
+  if (message === 'invalid token') {
+    return { status: 401, message: 'not authorized' };
+  }
+  return { status: 500, message };
+};
+
+module.exports = customErrorMessage;
