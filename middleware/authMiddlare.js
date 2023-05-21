ef9366a6b64cf702eb7907dcd4fe31c0bbdf5126
Yuriy Gomenyuk <yourgo@gmail.com>
2023-05-21 09:34:29 +0300

    ADD: auth middleware

---
+++ middleware/authMiddlare.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+// const { User } = require('../db/usersModel');
+const { JWT_SECRET } = process.env;
+
+const authMiddleware = (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(400).json({ message: 'Please, provide a token' });
+  }
+  const [, token] = req.headers.authorization.split(' ');
+
+  jwt.verify(token, JWT_SECRET);
+
+  return next();
+};
+
+module.exports = {
+  authMiddleware,
+};
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const { getUsersController } = require('../controllers/getUsersController');
 const { updateUserController } = require('../controllers/updateUserController');
+const { authMiddleware } = require('../middleware/authMiddlare');
 // const { updateValidateMiddleware } = require('../middleware/followingValidateMiddleware');
 
 const router = express.Router();
 
-// router.patch('/', updateValidateMiddleware);
-
-router.get('/:page', getUsersController);
-
-router.patch('/update/:userId', updateUserController);
+router.get('/:page', authMiddleware, getUsersController);
+// router.get('/:page', getUsersController);
+router.patch('/update/:userId', authMiddleware, updateUserController);
+// router.patch('/update/:userId', updateUserController);
 
 module.exports = router;
